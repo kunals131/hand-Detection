844166e479567f1bd9c554149b7ec84e574824c4
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-11-06 00:33:28 +0530

    Added hand detection and fingerpose with images to respond to

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
+import * as fp from 'fingerpose'
 import "./App.css";
+import thumbs_up from './thumbs_up.png'
+import thumbs_down from './thumbs_down.png'
+import victory from './victory.png'
 import { drawHand } from "./utilities";
+
+
+const imgStyle = {
+  position: "absolute",
+  marginLeft: "auto",
+  marginRight: "auto",
+  left: 400,
+  bottom: 500,
+  right: 0,
+  textAlign: "center",
+  height: 100,
+}
+
+const signs = { thumbs_up: thumbs_up, victory: victory, thumbs_down : thumbs_down };
+
 
 function App() {
   const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
+  const canvasRef = useRef(null)
+  
+  const [emoji, setEmoji] = useState(null);
+
   const runHandpose = async () => {
     const net = await handpose.load();
-    console.log("handpose model loaded!");
+    // console.log("handpose model loaded!");
     //Loop and detect Hands
     setInterval(()=>{
       detect(net);
     }, 100)
   };
+
+  //finger 
 
   const detect = async (net) => {
     //check data is available
@@ -39,7 +63,33 @@ function App() {
 
       //make detection
       const hand = await net.estimateHands(video);
+      // console.log(hand);
 
+      //New gesture :
+      const thumbsDownGesture = new fp.GestureDescription('thumbs_down');
+      thumbsDownGesture.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl);
+      thumbsDownGesture.addDirection(fp.Finger.Thumb, fp.FingerDirection.VerticalDown, 1.0);
+
+      thumbsDownGesture.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalDownLeft, 0.9);
+      thumbsDownGesture.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalDownRight, 0.9);
+
+      if (hand.length>0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.VictoryGesture,
+          fp.Gestures.ThumbsUpGesture,
+          thumbsDownGesture
+         
+
+
+        ])
+        const gesture = await GE.estimate(hand[0].landmarks,8);
+        console.log(gesture);
+        if (gesture.gestures!==undefined && gesture.gestures.length>0) {
+          // alert(signs[gesture.gestures[0].name])
+          setEmoji(signs[gesture.gestures[0].name]);
+
+        }
+      }
 
       //draw mesh
       const ctx = canvasRef.current.getContext("2d");
@@ -78,6 +128,11 @@ function App() {
             height: 480,
           }}
         />
+        {
+          emoji!==null ? <img src={emoji} style={{imgStyle}}/> : "" 
+        }
+
+        
       </header>
     </div>
   );
